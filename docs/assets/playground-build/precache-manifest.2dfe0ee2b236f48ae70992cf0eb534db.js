a18676e6af58bc9d86f116f1a6c5d2488c5f5900
mkolesnikov <mishkolesnikov@gmail.com>
2020-05-14 01:10:11 +0300

    Auto-generated commit

---
+++ docs/assets/playground-build/precache-manifest.2dfe0ee2b236f48ae70992cf0eb534db.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b210f3acdf8059ed275",
+    "url": "static/js/app.b7e1aa76.chunk.js"
+  },
+  {
+    "revision": "968dd11ad1a0a4cf3e70",
+    "url": "static/js/runtime~app.57f5bbf7.js"
+  },
+  {
+    "revision": "3c097cb3e9cae63dc1fa",
+    "url": "static/js/2.c0ac8024.chunk.js"
+  },
+  {
+    "revision": "a78e4b51bf5dd2e05c7453e1b5194d45",
+    "url": "static/media/icon.a78e4b51.png"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "expo-service-worker.js"
+  },
+  {
+    "revision": "939c5b5a66b790a0d0ee43c180cbbe1c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "87ec8e733975350b67dea893774af8b8",
+    "url": "index.html"
+  },
+  {
+    "revision": "67bda93997085eb5f45fc50ab92cc254",
+    "url": "static/js/runtime~app.57f5bbf7.js.gz"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "expo-service-worker.js.gz"
+  },
+  {
+    "revision": "30516dbea3bb937640ade5ca074787e6",
+    "url": "static/js/app.b7e1aa76.chunk.js.gz"
+  },
+  {
+    "revision": "bdf519f12e891a32646cc4506f61fd30",
+    "url": "static/js/2.c0ac8024.chunk.js.gz"
+  }
+];
